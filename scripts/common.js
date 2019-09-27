4d3b65a42f2cd38e467871dc69f4428c8f50099e
salegorka <egorsalomatin567@gmail.com>
2019-09-27 15:05:11 +0300

    Дз_1

--- scripts/common.js
+++ scripts/common.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+
+let tempCelsius = prompt("Введите температуру в цельсиях");
+
+let tempFahr = (9 / 5) * tempCelsius + 32;
+
+console.log("Температура в фаренгейтах: " + tempFahr + " \u00B0F");
+
+let name = "Василий";
+let admin = name;
+
+console.log(admin);
+
