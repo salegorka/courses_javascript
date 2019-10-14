bcbcd484b8530d4118152364ab8ea5bf4a0232f6
salegorka <egorsalomatin567@gmail.com>
2019-10-14 13:18:44 +0300

    Законченная галерея

--- scripts/gal.js
+++ scripts/gal.js
@@ -1,29 +1,59 @@
 // Галерея
 
-let currentImage = 1;
-
-
 function gallery() {
+    let currentImage = 1;
     let mainIMG = document.querySelector(".gal__main-image");
+    let roulete = document.querySelector(".gal__roulete");
     let rouleteIMGS = [...document.querySelectorAll(".gal__roulete-image")];
 
-    for (el of rouleteIMGS) {
-        el.addEventListener( "click", clickOnIMG);
-    }
+    document.querySelector(".gal__main").addEventListener("click", clickOnBtn);
+    roulete.addEventListener("click", clickOnIMG);
 
     function clickOnIMG (event) {
-        
-        let selectedElement = document.querySelector(".gal__roulete-image_selected");
 
-        if (selectedElement !== null) {
+        if (event.target.classList.contains("gal__roulete-image")) {
+        
+            let selectedElement = document.querySelector(".gal__roulete-image_selected");
             selectedElement.classList.remove("gal__roulete-image_selected");
+            event.target.classList.add("gal__roulete-image_selected");
+
+            mainIMG.src = event.target.src;
+
+            currentImage = +event.target.dataset["number"];
+
         }
 
-        event.currentTarget.classList.add("gal__roulete-image_selected");
+    }
 
-        let path = event.currentTarget.getAttribute("src");
-        mainIMG.setAttribute("src", path);
+    function clickOnBtn(event) {
 
+        if (event.target.dataset["direction"] == "left") {
+
+            currentImage--;
+            if (currentImage == 0) {
+                currentImage = 8;
+            }
+
+            let selectedElement = document.querySelector(".gal__roulete-image_selected");
+            selectedElement.classList.remove("gal__roulete-image_selected");
+            rouleteIMGS[currentImage - 1].classList.add("gal__roulete-image_selected");
+
+            mainIMG.src = rouleteIMGS[currentImage - 1].src;
+
+        } else if (event.target.dataset["direction"] == "right") {
+
+            currentImage++;
+            if (currentImage == 9) {
+                currentImage = 1;
+            }
+
+            let selectedElement = document.querySelector(".gal__roulete-image_selected");
+            selectedElement.classList.remove("gal__roulete-image_selected");
+            rouleteIMGS[currentImage - 1].classList.add("gal__roulete-image_selected");
+
+            mainIMG.src = rouleteIMGS[currentImage - 1].src;
+
+        }
     }
 
 }
