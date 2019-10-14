43f89f800823137b9716236e4d46676906380a98
salegorka <egorsalomatin567@gmail.com>
2019-10-14 14:10:38 +0300

    Задание распаковать объект

--- scripts/common.js
+++ scripts/common.js
@@ -1,2 +1,44 @@
 // Задание разобрать объект
 
+   obj = {
+ 	    a: 1,
+ 	    b: [
+		    {
+ 			    a: 2,
+			    b: [
+ 				    {a: 3, c: 'qwe1'}, {a: 3, c: 'qwe2'}, {a: 3, c: 'qwe3'}
+                ] 		
+            },
+		    {
+			    a: 2,
+			    b: [
+ 				    {a: 3, c: 'qwer1'}, {a: 3, c: 'qwer2'}
+ 			    ]
+ 		    },
+		    {
+ 			    a: 2,
+ 			    b: [
+ 				    {a: 3, c: 'qwert1'}
+ 			    ]
+ 		    }
+ 	    ]
+    }
+
+    ArrayOfLeafs = []
+
+function unpack(ob) {
+    if (ob.b !== undefined) {
+        for (el of ob.b) {
+            unpack(el);
+        }
+    } else {
+        ArrayOfLeafs.push({
+            c: ob.c,
+        });
+    }
+
+}
+
+unpack(obj);
+
+console.log(ArrayOfLeafs);
